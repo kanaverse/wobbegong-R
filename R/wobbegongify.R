15aa6684bf749cd20a943973b8a67bdf41dc4ea5
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-12-04 13:08:07 -0800

    Documented how some assays/columns are skipped during conversion.

--- R/wobbegongify.R
+++ R/wobbegongify.R
@@ -2,12 +2,19 @@
 #'
 #' Dump an object to disk in the \pkg{wobbegong} format for easy HTTP range requests.
 #'
-#' @param x A supported R object, typically a SummarizedExperiment or an instance of one of its subclasses.
+#' @param x A supported R object, typically a \link[SummarizedExperiment]{SummarizedExperiment} or an instance of one of its subclasses.
 #' @param path String containing a path to the directory to dump \code{x}.
 #' @param ... Additional arguments for specific methods.
 #' @param SummarizedExperiment.assay.check Function that accepts the index of the assay, the name of the assay, and the assay matrix.
 #' It should return a logical scalar specifying whether to save the assay; if \code{FALSE}, the assay is skipped.
 #' If \code{NULL}, no assays are skipped.
+#'
+#' @details
+#' For SummarizedExperiment objects, assays will be skipped if they are not 2-dimensional matrix-like objects of integer, logical or numeric type (according to \code{\link[DelayedArray]{type}}).
+#' Assays will also be skipped if \code{SummarizedExperiment.assay.check} is supplied and does not return \code{TRUE} when called with the assay's details.
+#'
+#' When passing \link[S4Vectors]{DataFrame} objects as \code{x}, columns will be skipped if they are not atomic (i.e., integer, logical, numeric or character).
+#' Factors will be automatically converted into character vectors.
 #'
 #' @return \code{path} is populated with the contents of \code{x}.
 #' \code{NULL} is returned invisibly.

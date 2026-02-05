1e61e2dfc1f5c3fcac320ecbc15f5e279067d120
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2026-02-05 12:22:45 -0800

    byteme zlib reader uses options

--- src/decompress.cpp
+++ src/decompress.cpp
@@ -7,9 +7,11 @@
 Rcpp::RawVector decompress(Rcpp::RawVector input) {
    auto ptr = static_cast<const unsigned char*>(input.begin());
    std::vector<unsigned char> output;
-   byteme::ZlibBufferReader reader(ptr, input.size(), /* mode = */ 0);
+   byteme::ZlibBufferReaderOptions opt;
+   opt.mode = 0;
+   byteme::ZlibBufferReader reader(ptr, input.size(), opt);
    while (reader.load()) {
-       output.insert(output.end(), reader.buffer(), reader.buffer() + reader.available());
+      output.insert(output.end(), reader.buffer(), reader.buffer() + reader.available());
    }
    return Rcpp::RawVector(output.begin(), output.end());
 }
